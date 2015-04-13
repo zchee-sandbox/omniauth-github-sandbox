5622bb376717df2638c1931b6708be80708672f0
zhee <zcheeee@gmail.com>
2015-04-13 12:59:29 +0900

    Add Home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  # noinspection RailsParamDefResolve
+  before_action :authenticate_user!, only: :show
+
+
+  def index
+  end
+
+
+  def show
+  end
+end
