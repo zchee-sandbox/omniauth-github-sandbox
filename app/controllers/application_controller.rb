14b9e1c76c7cfe16223853a781cefc9bee6a3be5
zhee <zcheeee@gmail.com>
2015-04-13 13:26:17 +0900

    Approval devise param when in,up,update for 'Unpermitted parameters'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # deviceのコントローラーのときに、下記のメソッドを呼ぶ
+  # noinspection RailsParamDefResolve
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # sign_inのときに、usernameも許可する
+    devise_parameter_sanitizer.for(:sign_in) << :email
+    # sign_upのときに、usernameも許可する
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    #  account_updateのときに、usernameも許可する
+    devise_parameter_sanitizer.for(:account_update) << :email
+  end
 end
