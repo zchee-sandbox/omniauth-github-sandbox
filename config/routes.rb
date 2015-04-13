85518d15a7477f305080820921120aa6cd2291aa
zhee <zcheeee@gmail.com>
2015-04-13 17:39:54 +0900

    Add Omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
+             controllers:        { omniauth_callbacks: 'omniauth_callbacks' }
   get 'home/index'
 
   get 'home/show'
