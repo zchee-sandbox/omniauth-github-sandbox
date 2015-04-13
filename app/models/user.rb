57e2bce59dae95c3bb466e8e8e023af33a4a764d
zhee <zcheeee@gmail.com>
2015-04-13 17:31:28 +0900

    Add Omniauth User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,24 @@
 class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:twitter]
 
   validates :uid, presence: true, uniqueness: true
+
+
+  def self.from_omniauth(auth)
+    # providerとuidでUserレコードを取得する
+    # 存在しない場合は、ブロック内のコードを実行して作成する
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      # auth.provider には "twitter"、
+      # auth.uidには twitterアカウントに基づいた個別のIDが入っている
+      # first_or_createメソッドが自動でproviderとuidを設定してくれるので、
+      # ここでは設定は必要ない
+      user.name  = auth.info.nickname # twitterで利用している名前が入る
+      user.email = auth.info.email # twitterの場合入らない
+    end
+  end
 
 end
