0baef48354ac77571244b7ab4ba3390ffba5ec02
zhee <zcheeee@gmail.com>
2015-04-16 19:27:00 +0900

    Undesirable RESTful of devise route, 'users' to 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
-             controllers:        { omniauth_callbacks: 'omniauth_callbacks' }
-  get 'home/index'
+  root 'home#show'
 
+  get 'home/index'
   get 'home/show'
 
-  root to: 'home#index'
-
-  # devise_scope :user do
-  #   root :to => "devise/sessions#new"
-  # end
+  devise_for :user,
+             # only:        [:sign_in, :sign_out, :session, :edit],
+             path_names:  { sign_in: 'login', sign_out: 'logout' },
+             controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
